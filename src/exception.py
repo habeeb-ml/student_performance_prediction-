de05dc7d748eaff1b04e3e2db683210ed08affd9
Habeeb O. Issa <habeebissa023@gmail.com>
2026-04-03 09:37:33 +0100

    'Logging

---
+++ src/exception.py
@@ -0,0 +1,28 @@
+import sys
+import logging
+from types import ModuleType
+
+def error_message_detail(error, error_detail: ModuleType):
+    _, _, exc_tb = error_detail.exc_info()
+    
+    # Ensure exc_tb is not None before accessing attributes
+    file_name = "Unknown"
+    line_number = 0
+    if exc_tb is not None:
+        file_name = exc_tb.tb_frame.f_code.co_filename
+        line_number = exc_tb.tb_lineno
+
+    error_message = "Error occurred in python script name [{0}] line number [{1}] error message [{2}]".format(
+        file_name, line_number, str(error)
+    )
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self, error_message, error_detail: ModuleType):
+        super().__init__(error_message)
+        self.error_message = error_message_detail(error_message, error_detail=error_detail)
+
+    def __str__(self):
+        return self.error_message
+
