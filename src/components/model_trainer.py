9c55fcdf643049bf71c91318992ea45e7947f0ed
Habeeb O. Issa <habeebissa023@gmail.com>
2026-04-06 15:22:21 +0100

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -52,27 +52,27 @@ class ModelTrainer:
             params={
                 "Decision Tree": {
                     'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    'splitter':['best','random'],
-                    'max_features':['sqrt','log2'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
                 },
                 "Random Forest":{
-                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
-                    'max_features':['sqrt','log2',None],
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Gradient Boosting":{
-                    'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
                     'learning_rate':[.1,.01,.05,.001],
                     'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
-                    'criterion':['squared_error', 'friedman_mse'],
-                    'max_features':['auto','sqrt','log2'],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
                     'n_estimators': [8,16,32,64,128,256]
                 },
                 "Linear Regression":{},
                 "K-Neighbors Regressor":{
                     'n_neighbors':[5,7,9,11],
-                    'weights':['uniform','distance'],
-                    'metric':['euclidean','manhattan']
+                    # 'weights':['uniform','distance'],
+                    # 'metric':['euclidean','manhattan']
                 },
                 "XGBRegressor":{
                     'learning_rate':[.1,.01,.05,.001],
@@ -85,7 +85,7 @@ class ModelTrainer:
                 },
                 "AdaBoost Regressor":{
                     'learning_rate':[.1,.01,0.5,.001],
-                    'loss':['linear','square','exponential'],
+                    # 'loss':['linear','square','exponential'],
                     'n_estimators': [8,16,32,64,128,256]
                 }
             }
